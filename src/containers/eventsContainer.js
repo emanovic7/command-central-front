13d5ee13f1fe33068cb782276c5fa7204e806896
github username <github email address>
2019-09-29 20:53:49 -0400

    add create event

--- src/containers/eventsContainer.js
+++ src/containers/eventsContainer.js
@@ -12,7 +12,11 @@ class EventsContainer extends Component {
     super();
     const now = new Date();
     this.state = {
-      events: []
+      events: [],
+      title: '',
+      date: '',
+      time: '',
+      allDay: null
     }
   }
 
@@ -24,17 +28,77 @@ class EventsContainer extends Component {
     }))
   }
 
+  addEvent = () => {
+    let user_id = this.props.user_id
+    fetch('http://localhost:3000/events', {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        title: this.state.title,
+        date: this.state.date,
+        time: this.state.time,
+        allDay: this.state.allDay,
+        user_id: user_id
+      })
+    })
+    .then(res => res.json())
+    .then(event => console.log(event))
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.addEvent()
+    this.setState({
+      title: '',
+      date: '',
+      allDay: null,
+      time: ''
+    })
+  }
+
   render(){
     console.log(this.state.events)
     return(
-      <div style={{ height: '500pt'}}>
-        <Calendar
-          events={this.state.events}
-          localizer={localizer}
-          startAccessor="start"
-          endAccessor="end"
-          defaultDate={moment().toDate()}
-        />
+
+      <div>
+        <div style={{ height: '300pt', width: '400pt'}}>
+          <Calendar
+            events={this.state.events}
+            localizer={localizer}
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={moment().toDate()}
+          />
+        </div>
+        <br /><br />
+        <div>
+          <h3>Add event</h3>
+          <form onSubmit={this.handleSubmit}>
+            <label >What's the event?</label>
+            <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/><br />
+
+            <label >When?</label>
+            <input type="date" name="date" value={this.state.date} onChange={this.handleChange}/><br />
+
+            <label >What's the time frame?</label>
+            <input type="time" name="time" value={this.state.time} onChange={this.handleChange}/><br />
+
+            <label >All day?</label>
+            <input type="boolean" name="allDay" value={this.state.allDay} onChange={this.handleChange}/><br />
+
+            <input type="submit" value="add event"/>
+          </form>
+        </div>
+
       </div>
     )
   }
