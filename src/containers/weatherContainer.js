858e25bd14a1e62a1ffde67afb7bf1c9faa3099d
github username <github email address>
2019-10-01 20:51:45 -0400

    add five day forecast to weather

--- src/containers/weatherContainer.js
+++ src/containers/weatherContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Weather from '../components/weather/Weather';
-import FiveDayWeather from '../components/weather/Weather';
+import FiveDayWeather from '../components/weather/fiveDay';
 import WeatherBoard from '../components/weather/weatherBoard';
 
 
@@ -12,36 +12,40 @@ class WeatherContainer extends Component {
     this.state = {
       weather: {},
       currently: {},
-      daily: {}
+      daily: []
     }
   }
 
 
-  // componentDidMount(){
-  //   fetch(`${'https://cors-anywhere.herokuapp.com/'}https://api.darksky.net/forecast/8a184aa8519f4a7c7742386e6cf699d8/42.3601,-71.0589?exclude=minutely,flags,hourly`)
-  //   .then(response => response.json())
-  //   .then(data => this.setState({
-  //     weather: data,
-  //     currently: data.currently,
-  //     daily: data.daily
-  //   }))
-  // }
+  componentDidMount(){
+    fetch(`${'https://cors-anywhere.herokuapp.com/'}https://api.darksky.net/forecast/8a184aa8519f4a7c7742386e6cf699d8/42.3601,-71.0589?exclude=minutely,flags,hourly`)
+    .then(response => response.json())
+    .then(data => this.setState({
+      weather: data,
+      currently: data.currently,
+      daily: data.daily.data
+    }))
+  }
 
   render(){
-    // const fiveDayWeather = Object.entries(this.state.daily).map((weather, idx) =>
-    //   <FiveDayWeather key={idx} weather={weather} />
-    // )
+    console.log(this.state.weather)
+    console.log(this.state.daily)
+    console.log(this.state.currently)
 
-
-    console.log(this.state)
+    const fiveDayForecast = this.state.daily.map(weather =>
+      <FiveDayWeather weather={weather} />
+    )
+    console.log(fiveDayForecast)
     return(
       <div>
         <h2>weather</h2>
+
         <Weather
           currently={this.state.currently}
         />
         <h2>Five Day Forecast</h2>
 
+        <div><FiveDayWeather weather={this.state.daily} /></div>
       </div>
     )
   }
