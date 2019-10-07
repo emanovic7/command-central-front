ea07547fa7f5aa866f88ef7e8aafb7612f3881f6
github username <github email address>
2019-10-07 14:22:28 -0400

    remove express file from App.js

--- src/App.js
+++ src/App.js
@@ -32,16 +32,6 @@ import ReservationsContainer from './containers/reservationsContainer';
 import FavoritesContainer from './containers/favoritesContainer';
 
 
-const express = require('express');
-const http = require('http');
-const path = require('path');
-let app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8080';
-app.set('port', port);
-const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
-
 
 
 class App extends Component {
