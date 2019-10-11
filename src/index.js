87e4bc8148326728f0480a316d916127e0160bab
github username <github email address>
2019-10-11 15:49:12 -0400

    import react-redux, refactor code and move store to its own folder, move store call to index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers/rootReducer'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom'
+import { createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,23 @@
+
+export default (state={user: "emanovic7"}, action) => {
+
+  console.log("state ", state)
+  console.log("nextState ", action)
+
+  switch (action.type) {
+    case "SET_USER":
+      return { user: action.user }
+    case "MAKE_GENESIS":
+      return { user: "Genesis" }
+
+    default:
+      return state
+  }
+
+  if(action.type === "SET_USER"){
+    return { user: action.user }
+  }else {
+    return { user: "Emoney"}
+  }
+
+}
