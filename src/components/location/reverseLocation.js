a626c4667c7910a0b70a8360a32b2b1610bd69e7
emanovic7 <e_owusu@live.com>
2019-12-20 15:57:49 -0500

    refactor reducers to use combine reducers, separate users, geolocation and reverseLocation reducers

--- src/components/location/reverseLocation.js
+++ src/components/location/reverseLocation.js
@@ -1,6 +1,5 @@
-
-
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class ReverseLocation extends Component {
@@ -15,18 +14,20 @@ class ReverseLocation extends Component {
   componentDidMount(){
     const API_KEY = "AIzaSyDlFzVIkqtTEuuhFi5ACR6OVx-YbtkVWOc";
 
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.8356724,-73.9269466&result_type=neighborhood&key=${API_KEY}`)
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=40.7443057,-73.996217&result_type=neighborhood&key=${API_KEY}`)
     .then(response => response.json())
-    .then(data => this.setState({
-      location: data.results[0].formatted_address
-    }))
+    // .then(data => this.setState({
+    //   location: data.results[0].formatted_address
+    // }))
     // .then(data => console.log(data.results[0].formatted_address))
+    // .then(location => this.props.setLocation(location))
   }
 
 
 
 
   render(){
+    console.log("reverseLocation props", this.props)
     return (
       <div>
         <p> location: {this.state.location} </p>
@@ -36,4 +37,14 @@ class ReverseLocation extends Component {
 }
 
 
-export default ReverseLocation;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setLocation: (location) => {
+      dispatch({type: 'SET_LOCATION', location: location})
+    }
+  }
+}
+
+
+
+export default connect(null, mapDispatchToProps)(ReverseLocation);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,19 @@
+import { combineReducers } from 'redux';
 
-export default (state={user: '', latitude: ''}, action) => {
+
+const rootReducer = combineReducers({
+  user: usersReducer,
+  geolocation: geolocationReducer,
+  reverseLocation: reverseLocationReducer
+})
+
+
+
+export default (
+  state = {
+    user: '',
+    latitude: ''
+  }, action) => {
 
   console.log("state from root", state)
   console.log("nextState ", action)
@@ -10,6 +24,10 @@ export default (state={user: '', latitude: ''}, action) => {
 
     case "SET_LATITUDE":
       return { latitude: action.latitude}
+
+    case "SET_LOCATION":
+      debugger
+        return {location: action.location}
 
     default:
       return state
