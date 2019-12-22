46892818fdb157630616265c5c9f3d929343f7c8
emanovic7 <e_owusu@live.com>
2019-12-22 15:57:10 -0500

    add thunk and compose to index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import ReverseLocation from './components/location/reverseLocation';
 import rootReducer from './reducers/rootReducer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
