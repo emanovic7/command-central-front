3227bb703c1e2075038bf5097a5e708d4248fc28
emanovic7 <e_owusu@live.com>
2019-12-22 23:18:55 -0500

    install thunk

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import rootReducer from './reducers/rootReducer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
