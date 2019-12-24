9e2a3f10fb510942110f4e5a693bab227ff2688c
emanovic7 <e_owusu@live.com>
2019-12-24 16:18:00 -0500

    change back to return action.user for ADD_USER

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,20 +1,20 @@
 
 const defaultState = {
   id: null,
+  name: '',
   username: ''
 }
 
 
 
 export default(state = defaultState, action) => {
-  debugger;
   switch (action.type) {
     case "SET_USER":
       return action.user
     case "LOGOUT":
       return defaultState
     case "ADD_USER":
-      return {...action.user}
+      return action.user
     default:
       return state;
   }
